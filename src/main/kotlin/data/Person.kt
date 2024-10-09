f4aa61a33a86f393ebb6d45bd36768902dc1f248
hafidjs <afidzpratama@gmail.com>
2024-10-09 15:31:28 +0700

    Function

--- src/main/kotlin/data/Person.kt
+++ src/main/kotlin/data/Person.kt
@@ -5,4 +5,16 @@ class Person {
     var firstName: String = ""
     var middleName: String? = null
     var lastName: String = ""
+
+    fun sayHello(name: String) {
+        println("Hello $name, My Name is $firstName")
+    }
+
+    fun run() {
+        println("I'm Run")
+    }
+
+    fun getFullName(): String {
+        return "$firstName $middleName $lastName"
+    }
 }
