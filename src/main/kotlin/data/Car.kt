6428bed765f59969fb4487197b8045d77f0688b6
hafidjs <afidzpratama@gmail.com>
2024-10-09 14:46:03 +0700

    Constructor

--- src/main/kotlin/data/Car.kt
+++ src/main/kotlin/data/Car.kt
@@ -1,7 +1,7 @@
 package data
 
-class Car {
+class Car(paramBrand: String, paramYear: Int = 2020) {
 
-    var brand: String = ""
-    var year: Int = 2020
+    var brand: String = paramBrand
+    var year: Int = paramYear
 }
