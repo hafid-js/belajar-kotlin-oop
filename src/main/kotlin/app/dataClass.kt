019ffe8ab04932c8cca32ae483f6fbb61e76e4c3
hafidjs <afidzpratama@gmail.com>
2024-10-10 22:40:25 +0700

    Data Class

---
+++ src/main/kotlin/app/dataClass.kt
@@ -0,0 +1,8 @@
+package app
+
+import data.Product
+
+fun main() {
+    val product = Product("Indomie", 2500, "Food")
+    println(product)
+}
---
+++ src/main/kotlin/data/Product.kt
@@ -0,0 +1,8 @@
+package data
+
+class Product (
+    val name: String,
+    val price: Int,
+    val category: String
+) {
+}
