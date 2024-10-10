a807ab0650e9bc1c744f3a6888925d2b2c70e3e9
hafidjs <afidzpratama@gmail.com>
2024-10-10 23:39:40 +0700

    Inner Class

---
+++ src/main/kotlin/app/innerClass.kt
@@ -0,0 +1,20 @@
+package app
+
+import data.Boss
+
+fun main() {
+    val boss1 = Boss("Eko")
+    val employee1 = boss1.Employee("Eko")
+    val employee2 = boss1.Employee("Joko")
+
+
+
+    val boss2 = Boss("Rudi")
+    val employee3 = boss2.Employee("Budi")
+    val employee4 = boss2.Employee("Joko")
+
+    employee1.hi()
+    employee2.hi()
+    employee3.hi()
+    employee4.hi()
+}
