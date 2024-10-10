9b955aa6c57179992fb6b86fa1529135101918cc
hafidjs <afidzpratama@gmail.com>
2024-10-11 05:39:49 +0700

    Singleton Object

---
+++ src/main/kotlin/app/singletonObject.kt
@@ -0,0 +1,22 @@
+package app
+
+import data.Application
+import data.Utilities
+
+fun main() {
+    Utilities.name = "Diubah"
+    println(Utilities.name)
+    println(Utilities.toUpper("Eko"))
+    a()
+    b()
+
+    println(Application.Utilities.toUpper("Eko"))
+}
+
+fun a() {
+    println(Utilities.name)
+}
+
+fun b() {
+    println(Utilities.name)
+}
